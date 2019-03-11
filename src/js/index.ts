85c043e833a610ae1c2082a595c923f65b8941ea
fisnik95 <36763178+fisnik95@users.noreply.github.com>
2019-03-11 14:40:40 +0100

    knap til at tilføje

--- src/js/index.ts
+++ src/js/index.ts
@@ -1,1 +1,21 @@
 //Her skal du skrive din typescriptkode
+document.getElementById('submitBtn').addEventListener("click",addvare);
+
+let elementInput = document.getElementById('nyvare');
+
+function addvare()
+{
+    let mytext = (<HTMLInputElement>elementInput).value;
+        
+    let usundliste = document.getElementById("listeUsund")
+    let newLi = document.createElement('li');
+    let newText = document.createTextNode(mytext)
+    newLi.appendChild(newText);
+    newLi.setAttribute('class','usundt');
+  
+    usundliste.appendChild(newLi);
+
+}
+
+
+
